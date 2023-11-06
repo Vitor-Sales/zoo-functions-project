ce7e1b511fe84bc8c3588c16278a5804fd07cea7
Vitor Sales <vitormalisa@gmail.com>
2023-11-06 16:36:31 -0300

    Requisito 3 concluido

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,8 +1,12 @@
 const data = require('../data/zoo_data');
 
-const getEmployeeByName = (employeeName) => data.employees
-  .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
+const getEmployeeByName = (employeeName) => {
+  const employeeObj = data.employees
+    .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
+  return !employeeObj ? {} : employeeObj;
+};
 
 console.log(getEmployeeByName('Nelson'));
+console.log(getEmployeeByName('Vitor'));
 
 module.exports = getEmployeeByName;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
-};
-
+  const checkIfManager = data.employees
+    .find((employee) => employee.id === id)
+    .managers.some((manager) => manager.length > 0);
+    return checkIfManager;
+}
 const getRelatedEmployees = (managerId) => {
   // seu código aqui
 };
 
+isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8');
+// console.log(data.employees.find((employee) => employee.id === '9e7d4524-363c-416a-8759-8aa7e50c0992').managers.some((manager) => manager.length > 0));
 module.exports = { isManager, getRelatedEmployees };
