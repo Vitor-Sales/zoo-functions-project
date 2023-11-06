e672693759f9576d6140124d8f8e6f56544ac4b2
Vitor Sales <vitormalisa@gmail.com>
2023-11-06 14:09:27 -0300

    Requisito 3 função acha o empregado existente. Falta dar o retorno desejado quando o empregado é inexistente

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,8 @@
 const data = require('../data/zoo_data');
 
-const getEmployeeByName = (employeeName) => {
-  // seu código aqui
-};
+const getEmployeeByName = (employeeName) => data.employees
+  .find(({ firstName, lastName }) => firstName === employeeName || lastName === employeeName);
+
+console.log(getEmployeeByName('Nelson'));
 
 module.exports = getEmployeeByName;
