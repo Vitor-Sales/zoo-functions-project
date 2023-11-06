e7f29825e0233d901e23fa071d7848668e44c237
Vitor Sales <vitormalisa@gmail.com>
2023-11-06 13:30:03 -0300

    Requisito 1 concluido

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,6 +2,7 @@ const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
   // seu código aqui
+  const checkedAge = data.species.filter((specie) => specie)
 };
 
 module.exports = getAnimalsOlderThan;
