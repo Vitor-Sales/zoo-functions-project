f72b227b8080aea562f674a1087b9b14dcc1a4d2
Vitor Sales <vitormalisa@gmail.com>
2023-11-06 13:50:56 -0300

    Requisito 2 refatorado

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,12 +1,9 @@
 const data = require('../data/zoo_data');
 
-const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
-  const findAnimal = data.species.find((specie) => specie.name === animal);
-  const checkAge = findAnimal.residents.every((spec) => spec.age >= age);
-  return checkAge;
-};
+const getAnimalsOlderThan = (animal, age) => data.species
+  .find((specie) => specie.name === animal)
+  .residents.every((spec) => spec.age >= age);
 
-console.log(getAnimalsOlderThan('lions', 4));
+// console.log(getAnimalsOlderThan('lions', 4));
 
 module.exports = getAnimalsOlderThan;
